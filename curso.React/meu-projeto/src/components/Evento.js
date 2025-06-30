36420939ef829140f3fd93ec1e17526f7c0f6221
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-06-30 16:30:53 -0300

    Aula 11 React - Passar eventod por props

--- curso.React/meu-projeto/src/components/Evento.js
+++ curso.React/meu-projeto/src/components/Evento.js
@@ -1,10 +1,18 @@
+import Button from "./evento/Button";
+
 function Evento() {
   function meuEvento() {
-    console.log('Opa, fui ativado')
+    console.log("Ativando primeiro evento");
+  }
+
+  function segundoEvento() {
+    console.log("Ativando o segundo evento");
   }
   return (
     <div>
       <p>Clique para disparar um evento</p>
+      <Button text="primeiro evento" event={meuEvento} />
+      <Button text="segundo evento" event={segundoEvento} />
       <button className="btn" onClick={meuEvento}>
         Ativar
       </button>
