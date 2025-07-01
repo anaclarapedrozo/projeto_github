16f6bcb8ccd8f17a3f8db3b027a0441878b5f706
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-07-01 13:21:01 -0300

    Aula 13 React - Renderização de listas

--- curso.React/meu-projeto/src/App.js
+++ curso.React/meu-projeto/src/App.js
@@ -7,13 +7,17 @@ import List from './components/List'
 import Evento from './components/Evento'
 import Form from './components/Form'
 import Condicional from './components/Condiconal';
+import OutraLista from './components/OutraLista';
 
 
 function App() {
+
+  const meusItens = ['CSS', 'React', 'JavaScript', 'MySQL']
   return (
     <div className="App">
-      <h1>Renderização Condicional</h1>
-      <Condicional/>
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={''}/>
     </div>
   );
 }
