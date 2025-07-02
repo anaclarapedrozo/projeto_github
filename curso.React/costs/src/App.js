4d225372a6b7825ab13de71da2eeb337080bdf6c
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-07-02 16:51:38 -0300

    Aula 19 React - Footer e Navbar

--- curso.React/costs/src/App.js
+++ curso.React/costs/src/App.js
@@ -4,25 +4,24 @@ import Contato from "./components/pages/Contato";
 import Empresa from "./components/pages/Empresa";
 import NewProject from "./components/pages/NewProject";
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Projects from "./components/pages/Projects";
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/contato">Contato</Link>
-        <Link to="/empresa">Empresa</Link>
-        <Link to="/newproject">Novo projeto</Link>
-      </ul>
+     <Navbar/>
       <Container customClass = 'min-heigth'>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/projects" element={<Projects />} />
           <Route path="/contato" element={<Contato />} />
           <Route path="/empresa" element={<Empresa />} />
           <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer/>
     </Router>
   );
 }
