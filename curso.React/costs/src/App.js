377d9a07dcd480bb35fc04b986badb34f9763523
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-07-02 14:04:41 -0300

    Aula 18 React - Estruturando o projeto

--- curso.React/costs/src/App.js
+++ curso.React/costs/src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contato from "./components/pages/Contato";
+import Empresa from "./components/pages/Empresa";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contato">Contato</Link>
+        <Link to="/empresa">Empresa</Link>
+        <Link to="/newproject">Novo projeto</Link>
+      </ul>
+      <Container customClass = 'min-heigth'>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/empresa" element={<Empresa />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
