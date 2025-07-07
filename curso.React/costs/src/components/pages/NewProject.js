ebf1f209605f0359d9f3d9234a981b5b1d442504
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-07-07 13:54:13 -0300

    Aula 24 React - Inserindo dados no banco via API

--- curso.React/costs/src/components/pages/NewProject.js
+++ curso.React/costs/src/components/pages/NewProject.js
@@ -1,12 +1,37 @@
-import ProjectForm from '../project/ProjectForm';
-import styles from './NewProjects.module.css'
+import ProjectForm from "../project/ProjectForm";
+
+import styles from "./NewProjects.module.css";
+
+import { Navigate, useNavigate } from "react-router-dom";
+
 
 function NewProject() {
+  const history = useNavigate();
+
+  function createPost(project) {
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data)
+        history('./Projects', {message: 'Projeto criado com sucesso!'})
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className={styles.newprojects_container}>
       <h1>Criar projeto</h1>
       <p>Crie o seu projeto para depois adicionar os serviços</p>
-      <ProjectForm/>
+      <ProjectForm handleSubmit={createPost} btnText='Criar Projeto'/>
     </div>
   );
 }
