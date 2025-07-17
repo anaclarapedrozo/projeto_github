573656dda904c554e58b1a56fb25093b601289b6
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-07-17 14:11:35 -0300

    Aula 26 React - Criando DashBoard

--- curso.React/costs/src/components/pages/Projects.js
+++ curso.React/costs/src/components/pages/Projects.js
@@ -1,18 +1,24 @@
 import Message from "../layout/Message";
 import { useLocation } from "react-router-dom";
+import Container from '../layout/Container'
+import styles from "./Projects.module.css";
+import LinkButtom from "../layout/LinkButtom";
 
 function Projects() {
-
-  const location = useLocation()
-  let message = ''
+  const location = useLocation();
+  let message = "";
   if (location.state) {
-    message = location.state.message
+    message = location.state.message;
   }
 
   return (
-    <div>
-      <h1>Meus projetos</h1>
-      {message && <Message type='success' msg={message}/>}
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus projetos</h1>
+        <LinkButtom to='/newproject' text='Criar Projeto'/>
+      </div>
+      {message && <Message type="success" msg={message} />}
+      <Container customClass='start'><p>Projetos...</p></Container>
     </div>
   );
 }
