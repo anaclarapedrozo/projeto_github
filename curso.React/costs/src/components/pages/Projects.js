aade76cbdefa4e1159c385a9e05218ea8ae905bc
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-07-21 12:15:18 -0300

    Aula 27 React - Resgatando projetos do banco de dados

--- curso.React/costs/src/components/pages/Projects.js
+++ curso.React/costs/src/components/pages/Projects.js
@@ -1,24 +1,53 @@
 import Message from "../layout/Message";
 import { useLocation } from "react-router-dom";
-import Container from '../layout/Container'
+import Container from "../layout/Container";
 import styles from "./Projects.module.css";
 import LinkButtom from "../layout/LinkButtom";
+import ProjectCard from "../project/ProjectCard";
+import { useState, useEffect } from "react";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
 
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className={styles.project_container}>
       <div className={styles.title_container}>
         <h1>Meus projetos</h1>
-        <LinkButtom to='/newproject' text='Criar Projeto'/>
+        <LinkButtom to="/newproject" text="Criar Projeto" />
       </div>
       {message && <Message type="success" msg={message} />}
-      <Container customClass='start'><p>Projetos...</p></Container>
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name || "sem nome"}
+              budget={project.budget}
+              category={project.category?.name || "Sem categoria"}
+              key={project.id}
+            />
+          ))}
+      </Container>
     </div>
   );
 }
